7ead5712b427f7cf8c45e8f326988386459dce10
Lotfi-Touil <lotfi.touil7@gmail.com>
2024-03-03 08:35:39 +0100

    limitons la casse sur le design de la plateforme...

--- src/Controller/Back/ProductCategoryController.php
+++ src/Controller/Back/ProductCategoryController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Back;
 use App\Entity\ProductCategory;
 use App\Form\ProductCategoryType;
 use App\Repository\ProductCategoryRepository;
+use App\Service\PageAccessService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,9 +15,18 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/platform/product/category')]
 class ProductCategoryController extends AbstractController
 {
-    #[Route('/', name: 'platform_product_category_index', methods: ['GET'])]
-    public function index(ProductCategoryRepository $productCategoryRepository): Response
+    private $pageAccessService;
+
+    public function __construct(PageAccessService $pageAccessService)
     {
+        $this->pageAccessService = $pageAccessService;
+    }
+
+    #[Route('/', name: 'platform_product_category_index', methods: ['GET'])]
+    public function index(Request $request, ProductCategoryRepository $productCategoryRepository): Response
+    {
+        $this->pageAccessService->checkAccess($request->attributes->get('_route'));
+
         return $this->render('back/product_category/index.html.twig', [
             'product_categories' => $productCategoryRepository->findAll(),
         ]);
@@ -25,6 +35,8 @@ class ProductCategoryController extends AbstractController
     #[Route('/new', name: 'platform_product_category_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $this->pageAccessService->checkAccess($request->attributes->get('_route'));
+
         $productCategory = new ProductCategory();
         $form = $this->createForm(ProductCategoryType::class, $productCategory);
         $form->handleRequest($request);
@@ -43,8 +55,10 @@ class ProductCategoryController extends AbstractController
     }
 
     #[Route('/{id}', name: 'platform_product_category_show', methods: ['GET'])]
-    public function show(ProductCategory $productCategory): Response
+    public function show(Request $request, ProductCategory $productCategory): Response
     {
+        $this->pageAccessService->checkAccess($request->attributes->get('_route'));
+
         return $this->render('back/product_category/show.html.twig', [
             'product_category' => $productCategory,
         ]);
@@ -53,6 +67,8 @@ class ProductCategoryController extends AbstractController
     #[Route('/{id}/edit', name: 'platform_product_category_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, ProductCategory $productCategory, EntityManagerInterface $entityManager): Response
     {
+        $this->pageAccessService->checkAccess($request->attributes->get('_route'));
+
         $form = $this->createForm(ProductCategoryType::class, $productCategory);
         $form->handleRequest($request);
 
@@ -71,6 +87,8 @@ class ProductCategoryController extends AbstractController
     #[Route('/{id}', name: 'platform_product_category_delete', methods: ['POST'])]
     public function delete(Request $request, ProductCategory $productCategory, EntityManagerInterface $entityManager): Response
     {
+        $this->pageAccessService->checkAccess($request->attributes->get('_route'));
+
         if ($this->isCsrfTokenValid('delete'.$productCategory->getId(), $request->request->get('_token'))) {
             $entityManager->remove($productCategory);
             $entityManager->flush();
